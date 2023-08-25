6dbf60c982cf5e1b6feb5c52c5bfcb90ddd61276
Umar <muhammadshafqatali3@gmail.com>
2023-08-26 00:53:18 +0500

    sdsd

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,7 +10,9 @@ function App() {
       <BrowserRouter>
         <Nav />
         <SideBar />
-        <Routes>{/* <Route path="/" element={<Home />} /> */}</Routes>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
       </BrowserRouter>
     </>
   );
