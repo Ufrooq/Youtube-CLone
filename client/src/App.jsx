749491a8d38446b5a6c443884a55f25444916cfd
Umar <muhammadshafqatali3@gmail.com>
2023-08-25 16:31:14 +0500

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,20 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="shadow-md rounded-md ring-1 ring-slate-100 w-4/12 mt-10 p-7 mx-auto text-center leading-7">
+        <h1 className="font-bold text-2xl">Box Creted</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis,
+          aliquid.
+        </p>
+        <button className="bg-sky-400 text-white px-3 py-1 rounded-md mt-2 shadow-md transition hover:bg-sky-500">
+          Toggle Theme
+        </button>
+      </div>
+    </>
+  );
+}
+
+export default App;
